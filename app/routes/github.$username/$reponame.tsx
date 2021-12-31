82d587c9d32622209f012171797061ca4f960e5a
Luis sejas <nativuks@gmail.com>
2021-12-31 18:11:10 -0400

    update reponame route

--- app/routes/github.$username/$reponame.tsx
+++ app/routes/github.$username/$reponame.tsx
@@ -1,11 +1,21 @@
 import { LoaderFunction, useLoaderData } from "remix";
-import { GithubApi } from "~/features/github";
+import { GithubApi, Types } from "~/features/github";
+import { Commits } from "~/features/Github/components/Commits";
 
-export const loader: LoaderFunction = async ({ params }) => {
-  return await GithubApi.getCommits(params.reponame, params.username);
+export const loader: LoaderFunction = async ({
+  params,
+}): Promise<Types.Commits.LoaderData> => {
+  return {
+    user: await GithubApi.getGithubUser(params.username),
+    commits: await GithubApi.getCommits(params.reponame, params.username),
+  };
 };
 
+export function ErrorBoundary() {
+  return <h3>Whoops. Something went wrong [Commits]</h3>;
+}
+
 export default function () {
-  const { repoName } = useLoaderData();
-  return <h3> {repoName}</h3>;
+  const { user, commits } = useLoaderData<Types.Commits.LoaderData>();
+  return <Commits commits={commits} user={user} />;
 }
